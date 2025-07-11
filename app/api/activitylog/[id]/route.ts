21be5e386a8e754864d95e57c2fad24000e13a05
curtilew <182793266+curtilew@users.noreply.github.com>
2025-07-11 17:43:56 -0400

    Fix Typescript errors

--- app/api/activitylog/[id]/route.ts
+++ app/api/activitylog/[id]/route.ts
@@ -2,6 +2,7 @@ import { getUserByClerkID } from "@/utils/auth"
 import { prisma } from "@/utils/db"
 import { NextResponse } from "next/server"
 
+// @ts-expect-error Database returns null but component expects undefined
 export const PATCH = async (request: Request, { params }) => {
     const { activityType, distance, date, note } = await request.json();
 
